a0d8f62239b6689fdd60539f797621d6f5781b6d
MarcoParizien <marcolivier.parisien@icloud.com>
2022-11-15 15:44:54 -0500

    vault

--- src/repositories/explorer.repo.js
+++ src/repositories/explorer.repo.js
@@ -29,7 +29,7 @@ class ExplorerRepository {
         vault = vault.toObject();
         delete vault._id;
 
-        return vault
+        return vault.vault
     }
 
     async retrieveExplorerExplorations(explorerEmail) {
