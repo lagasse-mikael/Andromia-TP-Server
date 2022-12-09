0716bdf0427ce8f3c235e47ac6dcdf672b615373
Amine K <1643859@cstj.qc.ca>
2022-12-08 22:50:58 -0500

    fix le model.

--- src/models/combatResult.model.js
+++ src/models/combatResult.model.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const combatResultSchema = mongoose.Schema({
     explorerUsername: { type:String,required: true },
-    explorerCreatureId: { type:[mongoose.Types.ObjectId], required:true},
-    foundCreatureId:{type:[mongoose.Types.ObjectId],required:true},
+    explorerCreatureId: { type: mongoose.Types.ObjectId, required:true,ref: 'Creature'},
+    foundCreatureId:{type: mongoose.Types.ObjectId,required:true, ref: 'Creature'},
     combatDate:{type:Date,required:true},
     userWon:{type:Boolean,required:true,default:false}
 },
