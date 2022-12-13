b5e84ceb48d7bb3b41e12c14419e75565f7ac6d5
mlagasse-cstj <2068410@cstj.qc.ca>
2022-12-13 15:10:18 -0500

    On recoit maintenant des inox et des elements au heure / aux 5 minutes !

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ import errorMiddleware from './src/middlewares/errors.js';
 import cors from 'cors';
 import explorerRepo from "./src/repositories/explorer.repo.js";
 import { guardRefreshToken } from "./src/middlewares/authorization.jwt.js";
+import explorerModel from "./src/models/explorer.model.js";
+import chalk from 'chalk';
+import random from "random";
 
 database();
 
@@ -24,21 +27,51 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(axios);
 
 // Tokens
-app.post("/refresh",guardRefreshToken, (req, res) => {
+app.post("/refresh", guardRefreshToken, (req, res) => {
     const refresh_token = req.body.refresh_token
-    if(!refresh_token)
-        return res.status(400).json({"errorMessage" : "Refresh token?"})
+    if (!refresh_token)
+        return res.status(400).json({ "errorMessage": "Refresh token?" })
 
     const associatedUser = explorerRepo.retrieveByID(refresh_token.userID)
-    if(!associatedUser)
-        return res.status(404).json({"errorMessage" : "Il provient d'où ton refresh token? (Possiblement invalide)"})
+    if (!associatedUser)
+        return res.status(404).json({ "errorMessage": "Il provient d'où ton refresh token? (Possiblement invalide)" })
 
     const tokens = explorerRepo.generateTokens(associatedUser.email, associatedUser._id)
-    
+
     return res.status(200).json(tokens)
+});
+
+// Inox increase.
+app.put("/increaseBalances", async (req, res) => {
+    try {
+        const explorers = await explorerModel.updateMany({
+            $inc: { "vault.inox": 2 }
+        })
+
+        return res.status(200).json(explorers)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ "message": err })
+    }
+
+});
+
+// Elements increase.
+app.put("/increaseElements", async (req, res) => {
+    try {
+        const randomNumber = random.int(1,3)
+        const explorers = await explorerModel.updateMany({
+            $inc: { "vault.elements.$[].quantity": randomNumber }
+        })
+
+        return res.status(200).json(explorers)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ "message": err })
+    }
+
 });
 
 // Routes
@@ -48,5 +81,27 @@ app.use("/elements", elementRoutes)
 app.use("/explorations", explorationRoutes)
 app.use("/combats", combatRoutes)
 
+// Middlewares
 app.use(errorMiddleware);
+
+// Fonctions recurrentes.
+const intervalInox = setInterval(async () => {
+    const response = await axios.put(`${process.env.BASE_URL}increaseBalances`)
+
+    if (response.status == 200) {
+        console.log(chalk.yellow(`🫰 Tout le monde a recu des inox! (+ 2)`));
+    } else {
+        console.log(response);
+    }
+}, 300000)
+
+const intervalElements = setInterval(async () => {
+    const response = await axios.put(`${process.env.BASE_URL}increaseElements`)
+
+    if (response.status == 200) {
+        console.log(chalk.cyan(`🔬 Tout le monde a recu des elements! (+ 1..3)`));
+    } else {
+        console.log(response);
+    }
+}, 1 * 60 * 60 * 1000)
 export default app;
