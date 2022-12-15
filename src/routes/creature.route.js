4460b6b1874b846f76f5b6fbdb08fd20f0402d91
mlagasse-cstj <2068410@cstj.qc.ca>
2022-12-15 14:24:57 -0500

    Nouvelle route pour aller chercher une creature par UUID

--- src/routes/creature.route.js
+++ src/routes/creature.route.js
@@ -10,6 +10,7 @@ class CreatureRoutes {
     constructor() {
         router.get('/', guardAuthJWT, this.getAll)
         router.get('/:creatureID', guardAuthJWT,this.getOne)
+        router.get('/getOneByUUID/:creatureUUID', guardAuthJWT,this.getOneByUUID)
     }
 
     async getAll(req, res, next) {
@@ -27,6 +28,18 @@ class CreatureRoutes {
     async getOne(req, res, next) {
         try {
             let creature = await creatureRepo.retrieveByID(req.params.creatureID)
+
+            creature = creatureRepo.transformObject(creature)
+
+            res.status(httpStatus.OK).json(creature)
+        } catch (err) {
+            return next(err)
+        }
+    }
+
+    async getOneByUUID(req, res, next) {
+        try {
+            let creature = await creatureRepo.retrieveByUUID(req.params.creatureUUID)
 
             creature = creatureRepo.transformObject(creature)
 
